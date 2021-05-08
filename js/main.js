ccf7b9c2d1a6dce94100299302bb89a51c54d329
mahim-2790 <mohi35-2790@diu.edu.bd>
2021-05-08 23:15:09 +0600

    rest is added

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function initMap() {
+    // Your location
+    const loc = { lat: -34.397, lng: 150.644 };
+    // Centered map on location
+    const map = new google.maps.Map(document.querySelector('.map'), {
+      zoom: 14,
+      center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map: map });
+  }
+
+  $('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
