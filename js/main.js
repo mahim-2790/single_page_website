cd5b774fb93db68cc5390d3ea6702b896ecbf969
mahim-2790 <mohi35-2790@diu.edu.bd>
2021-05-10 13:06:37 +0600

    rsposiveness for mobile added

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function initMap() {
     const loc = { lat: -34.397, lng: 150.644 };
     // Centered map on location
     const map = new google.maps.Map(document.querySelector('.map'), {
-      zoom: 14,
+      zoom: 8,
       center: loc
     });
     // The marker, positioned at location
